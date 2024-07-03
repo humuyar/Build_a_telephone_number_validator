140e3781c93a192f7ceeb602d47e2ac1671645c5
humuyar <humiq6071@gmail.com>
2024-07-03 02:35:18 -0700

    fix the error

--- script.js
+++ script.js
@@ -3,36 +3,24 @@ const checkBtn = document.getElementById('check-btn');
 const clearBtn = document.getElementById('clear-btn');
 const resultsDiv = document.getElementById('results-div');
 
+function checkValidNumber(input) {
   const countryCode = '^(1\\s?)?';
   const areaCode = '(\\([0-9]{3}\\)|[0-9]{3})';
   const spacesDashes = '[\\s\\-]?';
   const phoneNumber = '[0-9]{3}[\\s\\-]?[0-9]{4}$';
-  const phoneRegex = new (RegExp)(
-   `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`
-  );
-
-  const checkValidNumber = (input) => {
-    if (input === '') {
-      alert('Please provide a phone number');
-      return;
-  }
-
-  const pTag = document.createElement('p');
-  pTag.className = 'results-text';
-  phoneRegex.test(input)
-    ? (pTag.style.color = '#00471b')
-    : (pTag.style.color = '#4d3800');
-  pTag.appendChild(
-    document.createTextNode(
-      `${phoneRegex.test(input) ? 'Valid' : 'Invalid'} US number: ${input}`
-    ,)
-  );
-  resultsDiv.appendChild(pTag);
-};
+  const phoneRegex = new RegExp(`${countryCode}${areaCode}${spacesDashes}${phoneNumber}`);
+  resultsDiv.innerHTML = `${phoneRegex.test(input) ? 'Valid' : 'Invalid'} US number: ${input}`;
+}
 
 checkBtn.addEventListener('click', () => {
-  checkValidNumber(userInput.value);
-  userInput.value = '';
+  const input = userInput.value;
+
+  if (input.length === 0) {
+    alert('Please provide a phone number');
+  } else {
+    checkValidNumber(input);
+    userInput.value = '';
+  }
 });
 
 userInput.addEventListener('keydown', (e) => {
@@ -43,5 +31,5 @@ userInput.addEventListener('keydown', (e) => {
 });
 
 clearBtn.addEventListener('click', () => {
-  resultsDiv.textContent = '';
+  resultsDiv.innerHTML = '';
 });
